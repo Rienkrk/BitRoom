f743d1f1c6d3f4610d68795328800cbb6a2afa4e
Rienk Koenders Laptop <rienk.koenders@student.uva.nl>
2018-04-07 15:07:27 +0200

    Opzetten Routes/Templates

--- main.go
+++ main.go
@@ -1,9 +1,35 @@
 package main
 
-import "fmt"
+import "net/http"
+import "github.com/gorilla/mux"
+import "html/template"
+
+// Maak de templates voor alle handles bereikbaar
+var templates *template.Template
 
 func main() {
 
-  fmt.Printin("Base")
+  // Parse de templates
+  templates = template.Must(template.ParseGlob("templates/*.html"))
 
+  // Initialiseer de webserver
+  r := mux.NewRouter()
+
+  // Vertel de server waar de static files zijn
+  staticFileDirectory := http.Dir("./static/")
+	staticFileHandler := http.StripPrefix("/static/", http.FileServer(staticFileDirectory))
+	r.PathPrefix("/static/").Handler(staticFileHandler).Methods("GET")
+
+  // Routes
+  r.HandleFunc("/", indexHandler)
+
+  // Start de server
+  http.Handle("/", r)
+  http.ListenAndServe(":8080", r)
+
+}
+
+// Handler voor de index pagina
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+  templates.ExecuteTemplate(w, "index.html", nil)
 }
