d53098d8fd2340d73663b16a72ef3303988a70ed
Rienk Koenders Laptop <rienk.koenders@student.uva.nl>
2018-04-08 20:45:25 +0200

    Poging tot functionaliteit, opmaak

--- main.go
+++ main.go
@@ -4,11 +4,10 @@ import (
 	"fmt"
 	"html/template"
 	"net/http"
-
 	"github.com/gorilla/mux"
 )
 
-// Maak de templates voor alle handles bereikbaar
+// Maak de templates voor alle functies bereikbaar
 var templates *template.Template
 
 func main() {
@@ -16,7 +15,7 @@ func main() {
 	// Parse de templates
 	templates = template.Must(template.ParseGlob("templates/*.html"))
 
-	// Initialiseer de webserver
+	// Initialiseer de router
 	r := mux.NewRouter()
 
 	// Vertel de server waar de static files zijn
@@ -26,46 +25,66 @@ func main() {
 
 	// Views voor pagina's
 	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/login", loginHandler)
 	r.HandleFunc("/overview", overviewHandler)
 	r.HandleFunc("/portal", portalHandler)
 	r.HandleFunc("/settle", settleHandler)
 
 	// Views voor acties
 	r.HandleFunc("/register", registerHandler).Methods("POST")
+	r.HandleFunc("/addEntry", entryHandler).Methods("POST")
 
-	// Start de server
+	// Initialiseer de server
 	http.Handle("/", r)
 	http.ListenAndServe(":8080", r)
 
 }
 
-// Handler voor de index pagina
+// Handler voor entries
+func entryHandler(w http.ResponseWriter, r *http.Request) {
+	http.Redirect(w, r, "/overview", 302)
+}
+
+// Handler voor de landings pagina
 func indexHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "index.html", nil)
 }
 
-// Handler voor de overview pagina
-func overviewHandler(w http.ResponseWriter, r *http.Request) {
-	templates.ExecuteTemplate(w, "overview.html", nil)
+// Handler voor de login pagina
+func loginHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "login.html", nil)
 }
 
-// Handler voor de portal pagina
+// Handler voor de overzicht pagina
+func overviewHandler(w http.ResponseWriter, r *http.Request) {
+		templates.ExecuteTemplate(w, "overview.html", nil)
+}
+
+// Handler voor de portaal pagina
 func portalHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "portal.html", nil)
 }
 
-// Handler voor de settle pagina
+// Handler voor de vereken pagina
 func settleHandler(w http.ResponseWriter, r *http.Request) {
 	templates.ExecuteTemplate(w, "settle.html", nil)
 }
 
-// Handler voor de overview pagina
+// Handler voor het registreren
 func registerHandler(w http.ResponseWriter, r *http.Request) {
+
+	// Parse de form
 	r.ParseForm()
+
+	// Haal data uit de form
 	mail := r.PostForm.Get("mail")
 	name := r.PostForm.Get("name")
 	password := r.PostForm.Get("password")
 	passwordRepeat := r.PostForm.Get("passwordRepeat")
+
+	// Print naar console
 	fmt.Println(mail, name, password, passwordRepeat)
-	http.Redirect(w, r, "/overview", 302)
+
+	// Redirect naar de overview pagina
+	http.Redirect(w, r, "/portal", 302)
 }
