01a260917e860cc0f490702d898c80ba460a3a76
Rienk Koenders Laptop <rienk.koenders@student.uva.nl>
2018-04-08 09:27:47 +0200

    Views opmaak af voor landing/overzicht

--- main.go
+++ main.go
@@ -9,27 +9,34 @@ var templates *template.Template
 
 func main() {
 
-  // Parse de templates
-  templates = template.Must(template.ParseGlob("templates/*.html"))
+	// Parse de templates
+	templates = template.Must(template.ParseGlob("templates/*.html"))
 
-  // Initialiseer de webserver
-  r := mux.NewRouter()
+	// Initialiseer de webserver
+	r := mux.NewRouter()
 
-  // Vertel de server waar de static files zijn
-  staticFileDirectory := http.Dir("./static/")
+	// Vertel de server waar de static files zijn
+	staticFileDirectory := http.Dir("./static/")
 	staticFileHandler := http.StripPrefix("/static/", http.FileServer(staticFileDirectory))
 	r.PathPrefix("/static/").Handler(staticFileHandler).Methods("GET")
 
-  // Routes
-  r.HandleFunc("/", indexHandler)
+	// Routes
+	// r.HandleFunc("/overview", overviewHandler)
+	r.HandleFunc("/", indexHandler)
+	r.HandleFunc("/overview", overviewHandler)
 
-  // Start de server
-  http.Handle("/", r)
-  http.ListenAndServe(":8080", r)
+	// Start de server
+	http.Handle("/", r)
+	http.ListenAndServe(":8080", r)
 
 }
 
 // Handler voor de index pagina
 func indexHandler(w http.ResponseWriter, r *http.Request) {
-  templates.ExecuteTemplate(w, "index.html", nil)
+	templates.ExecuteTemplate(w, "index.html", nil)
+}
+
+// Handler voor de overview pagina
+func overviewHandler(w http.ResponseWriter, r *http.Request) {
+	templates.ExecuteTemplate(w, "overview.html", nil)
 }
